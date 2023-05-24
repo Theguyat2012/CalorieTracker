e010d20112be78921df254bae77ffac791c1ee1c
Alex Ho <ho.alex100@gmail.com>
2023-05-23 22:38:17 -0400

    change name to CalTrakr

--- components/Title.js
+++ components/Title.js
@@ -3,7 +3,7 @@ import { PixelRatio, StyleSheet, Text, View } from 'react-native';
 export default function Title() {
     return (
         <View style={styles.headerWrapper}>
-            <Text style={styles.headerText}>CalTrak</Text>
+            <Text style={styles.headerText}>CalTrakr</Text>
         </View>
     );
 }
